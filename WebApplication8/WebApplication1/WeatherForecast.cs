964865db038605e00e3cda9512a78e5899aad7c7
Davi Ji <v-weji1@microsoft.com>
2023-01-10 17:28:55 +0800

    afa

--- WebApplication8/WebApplication1/WeatherForecast.cs
+++ WebApplication8/WebApplication1/WeatherForecast.cs
@@ -17,4 +17,6 @@ public class WeatherForecast
     public string? NewProperty3 { get; set; }
 
     public string? NewProperty4 { get; set; }
+
+    public string? NewPropertynewadded { get; set; }
 }
