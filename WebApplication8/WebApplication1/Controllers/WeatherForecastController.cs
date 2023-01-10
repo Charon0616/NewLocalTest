2e6e93e926b1b6a653528a11e6cdca8fc73c6931
Davi Ji <v-weji1@microsoft.com>
2023-01-10 17:26:43 +0800

    commit and push to github

--- WebApplication8/WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication8/WebApplication1/Controllers/WeatherForecastController.cs
@@ -29,6 +29,7 @@ public class WeatherForecastController : ControllerBase
             NewProperty2 = Summaries[Random.Shared.Next(Summaries.Length)],
             NewProperty3 = Summaries[Random.Shared.Next(Summaries.Length)],
             NewProperty4 = Summaries[Random.Shared.Next(Summaries.Length)],
+            NewPropertynewadded = Summaries[Random.Shared.Next(Summaries.Length)]
         })
         .ToArray();
     }
